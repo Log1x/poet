5bd238b00123cde94ba09051d02c80e69f0252ab
Brandon <brandon@tendency.me>
2020-03-24 18:10:24 -0500

    bump

--- README.md
+++ README.md
@@ -4,9 +4,11 @@
 ![Build Status](https://img.shields.io/circleci/build/github/Log1x/poet?style=flat-square)
 ![Total Downloads](https://img.shields.io/packagist/dt/log1x/poet?style=flat-square)
 
-Poet provides simple configuration-based post type and taxonomy registration for Sage 10 utilizing [Extended CPTs](https://github.com/johnbillion/extended-cpts).
+Poet provides simple configuration-based post type and taxonomy registration as well as the ability to register Gutenberg blocks to be rendered with Laravel Blade.
 
-Poet also has the ability to modify an existing post type or taxonomy if it finds that it already exists.
+Post types and taxonomies are registered utilizing [Extended CPTs](https://github.com/johnbillion/extended-cpts).
+
+If the passed post type or taxonomy already exists, Poet will automatically modify their objects instead allowing easy manipulation of built-in post types/taxonomies.
 
 ## Requirements
 
@@ -30,13 +32,68 @@ Publish the example configuration using:
 $ wp acorn vendor:publish --provider="Log1x\Poet\PoetServiceProvider"
 ```
 
-For additional configuration values, see the [Extended CPTs](https://github.com/johnbillion/extended-cpts/wiki) and [`register_post_type()`](https://developer.wordpress.org/reference/functions/register_post_type/) documentation.
+### Creating a Post Type
 
-**Note**: Do not nest configuration in a `'config'` key like shown on Extended CPTs.
+All configuration related to Poet is located in `config/poet.php`. Here you will find an example Book post type pre-configured with a few common settings:
 
-#### Extending a Post Type or Taxonomy
+```php
+'post' => [
+    'book' => [
+        'enter_title_here' => 'Enter book title',
+        'menu_icon' => 'dashicons-book-alt',
+        'supports' => ['title', 'editor', 'author', 'revisions', 'thumbnail'],
+        'show_in_rest' => true,
+        'has_archive' => false,
+        'labels' => [
+            'singular' => 'Book',
+            'plural' => 'Books',
+        ],
+    ],
+],
+```
 
-Extending a post type or taxonomy is as simple as adding a new one. Simply just pass it arguments you would otherwise use while registering and Poet will do the rest.
+In it's simplest form, a post type can be created by simply passing a string.
+
+```php
+'post' => [
+    'book',
+],
+```
+
+To see additional configuration options for post types, take a look at [`register_post_type()`](https://developer.wordpress.org/reference/functions/register_post_type/) and [`register_extended_post_type()`](https://github.com/johnbillion/extended-cpts/wiki/Registering-Post-Types).
+
+> **Note**: Do not nest configuration in a `'config'` key like shown in the Extended CPTs documentation.
+
+## Creating a Taxonomy
+
+Creating a taxonomy is similar to a post type. Looking in `config/poet.php`, you will see a genre taxonomy accompanying the default book post type.
+
+```php
+'taxonomy' => [
+    'genre' => [
+        'links' => ['book'],
+        'meta_box' => 'radio',
+    ],
+],
+```
+
+The most relevent configuration value is `links` which defines the post-type the taxonomy is connected to. If no link is specified, it will default to `post`.
+
+In it's simplest form, you can simply pass a string to create a taxonomy for a post.
+
+```php
+'taxonomy' => [
+    'genre',
+],
+```
+
+To see additional configuration options for taxonomies, take a look at [`register_taxonomy()`](https://codex.wordpress.org/Function_Reference/register_taxonomy) and [`register_extended_taxonomy()`](https://github.com/johnbillion/extended-cpts/wiki/Registering-taxonomies).
+
+> **Note**: Do not nest configuration in a `'config'` key like shown in the Extended CPTs documentation.
+
+### Modifying an existing Post Type or Taxonomy
+
+Modifying an existing post type or taxonomy is similar to creating a new one. Simply pass the arguments you would otherwise use while registering and Poet will do the rest.
 
 Below is an example for enabling the built-in `wp_block` post type in the menu as well as assigning it a more fitting icon.
 
@@ -49,6 +106,12 @@ Below is an example for enabling the built-in `wp_block` post type in the menu a
     ],
 ],
 ```
+
+### Registering a Block
+
+Poet provides an easy way to register a Gutenberg block with the editor using an accompanying blade view for rendering the block on the frontend.
+
+Blocks are registered using the `namespace/label` defined when [registering the block with the editor](https://developer.wordpress.org/block-editor/developers/block-api/block-registration/#registerblocktype). If no namespace is provided, the current theme text domain will be used instead.
 
 ## Bug Reports
 
--- src/Poet.php
+++ src/Poet.php
@@ -4,104 +4,121 @@ namespace Log1x\Poet;
 
 use WP_Post_Type;
 use WP_Taxonomy;
+use Illuminate\Support\Str;
 
 class Poet
 {
     /**
-     * Returns the configured post types for the application.
+     * Returns the Poet configuration.
      *
      * @var array
      */
-    protected $post;
-
-    /**
-     * Returns the configured taxonomies for the application.
-     *
-     * @var array
-     */
-    protected $taxonomy;
-
-    /**
-     * Returns the configured blocks for the application.
-     *
-     * @var array
-     */
-    protected $block;
+    protected $config;
 
     /**
      * Create a new Poet instance.
      *
-     * @param  array $post
-     * @param  array $taxonomy
+     * @param  array $config
      * @return void
      */
-    public function __construct($post = [], $taxonomy = [], $block = [])
+    public function __construct($config)
     {
-        $this->post = $post;
-        $this->taxonomy = $taxonomy;
-        $this->block = $block;
+        $this->config = collect($config);
 
         add_action('init', function () {
-            $this->register();
+            $this->registerTypes();
+            $this->registerBlocks();
+        }, 20);
+    }
+
+    /**
+     * Register the configured post types and taxomonies using Extended CPTs.
+     *   ↪ https://github.com/johnbillion/extended-cpts
+     *
+     * If a post type / taxonomy already exists, the object will be modified instead.
+     *   ↪ https://codex.wordpress.org/Function_Reference/get_post_type_object
+     *   ↪ https://developer.wordpress.org/reference/functions/get_taxonomy/
+     *
+     * @return void
+     */
+    protected function registerTypes()
+    {
+        $this->config->only(['post', 'taxonomy'])->each(function ($value, $type) {
+            foreach ($value as $key => $config) {
+                $config = collect($config);
+
+                if ($this->exists($key)) {
+                    if ($config->isEmpty()) {
+                        return;
+                    }
+
+                    return $this->modify($key, $config->all());
+                }
+
+                if (Str::is($type, 'taxonomy')) {
+                    return register_extended_taxonomy(
+                        $key,
+                        $config->get('links', 'post'),
+                        $config->all(),
+                        $config->get('labels', [])
+                    );
+                }
+
+                return register_extended_post_type(
+                    $key,
+                    $config->all(),
+                    $config->get('labels', [])
+                );
+            }
         });
     }
 
     /**
-     * Register post types and taxonomies.
+     * Register the configured block types with the editor using Blade
+     * for rendering the registered block.
+     *
+     * If no namespace is provided on the block, Poet will default to
+     * the current theme text domain.
+     *
+     * Optionally, you may pass a block as an array containing:
+     *   ↪ attributes – An array of custom block attributes.
+     *   ↪ strip – When set to false, `$content` will always return true.
+     *
+     * Given the Block `sage/accordion`, the Block view would be located at:
+     *   ↪ `views/blocks/accordion.blade.php`
+     *
+     * Block views have the following variables available:
+     *   ↪ $data    – An object containing the block data.
+     *   ↪ $content – A string containing the InnerBlocks content.
+     *                Returns `false` when empty.
      *
      * @return void
      */
-    protected function register()
+    protected function registerBlocks()
     {
-        collect($this->post)
-            ->each(function ($config, $key) {
-                $config = $config ?? [];
+        return $this->config->only('block')->each(function ($config, $key) {
+            $config = collect($config);
 
-                if ($this->exists($key)) {
-                    return $this->modify($key, $config);
-                }
+            if (! Str::contains($key, '/')) {
+                $key = Str::start($key, $this->namespace());
+            }
 
-                register_extended_post_type($key, $config, $config['labels'] ?? []);
-            });
-
-        collect($this->taxonomy)
-            ->each(function ($config, $key) {
-                $config = $config ?? [];
-
-                if ($this->exists($key)) {
-                    return $this->modify($key, $config);
-                }
-
-                register_extended_taxonomy($key, $config['links'] ?? 'post', $config, $config['labels'] ?? []);
-            });
-
-        collect($this->block)
-            ->each(function ($config, $key) {
-                $config = collect($config ?? []);
-
-                if (! Str::contains($key, '/')) {
-                    $key = Str::start($key, 'block/');
-                }
-
-                register_block_type($key, [
-                    'render_callback' => function ($data, $content) use ($key, $config) {
-                        return view($config->get('view', 'blocks.' . Str::after($key, '/')), [
-                            'data' => (object) $data,
-                            'content' => $config->get('strip', true) ? $content :
-                                ! empty(
-                                    wp_strip_all_tags($content, true)
-                                )
-                            ? $content : null
-                        ]);
-                    },
-                    'attributes' => $config->get('attributes', [])
-                ]);
-            });
+            return register_block_type($key, [
+                'attributes' => $config->get('attributes', []),
+                'render_callback' => function ($data, $content) use ($key, $config) {
+                    return view($config->get('view', 'blocks.' . Str::after($key, '/')), [
+                        'data' => (object) $data,
+                        'content' => $config->get('strip', true) && $this->isEmpty($content) ? false : $content
+                    ]);
+                },
+            ]);
+        });
     }
 
     /**
      * Checks if a post type or taxonomy already exists.
      *
+     * @param  string $name
      * @return bool
      */
     protected function exists($name)
@@ -112,8 +129,9 @@ class Poet
     /**
      * Modifies an existing post type or taxonomy object.
      *
-     * @param string $name
-     * @param array  $config
+     * @param  string $name
+     * @param  array  $config
+     * @return void
      */
     protected function modify($name, $config)
     {
@@ -137,5 +155,31 @@ class Poet
     protected function verify($object)
     {
         return $object instanceof WP_Post_Type || $object instanceof WP_Taxonomy;
+    }
+
+    /**
+     * Use the current theme's text domain as a namespace.
+     *
+     * @param  string $delimiter
+     * @return string
+     */
+    protected function namespace($delimiter = '/')
+    {
+        return (Str::slug(
+            wp_get_theme()->get('text_domain')
+        ) ?? 'sage') . $delimiter;
+    }
+
+    /**
+     * Check if a string is empty after stripping tags and whitespace.
+     *
+     * @param  string $value
+     * @return bool
+     */
+    protected function isEmpty($value)
+    {
+        return empty(
+            wp_strip_all_tags($value, true)
+        );
     }
 }
